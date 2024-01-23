dea20657a5a9287f53501d4b708435e5c933630d
willykar <wilsonnjenga799@gmail.com>
2024-01-23 09:40:59 -0800

    update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,10 @@
 #include "sort.h"
 
 /*
- *  bubble_sort - A finction that sorts an array of integers
- *  in ascending order using Bubble sort
- *  array: Array to sort
- *  size: The size of the array
+ * bubble_sort - A finction that sorts an array of integers
+ * in ascending order using Bubble sort
+ * @array: Array to sort
+ * @size: The size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
